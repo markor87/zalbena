a1395486ce3c3e85ebd49d0d422a34df55b875f8
marko.radovanovic <mradovanovic@msn.com>
2025-11-07 10:39:32 +0100

    axios ispravke

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = '/api';
+window.axios.defaults.baseURL = '/zalbena/api';
 window.axios.defaults.withCredentials = true;
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Public routes
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 // Protected routes
 Route::middleware('auth:web')->group(function () {
